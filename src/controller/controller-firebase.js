6e98c36a429305713006845bb627bd9eb510fb40
anaisTe <angieanais@gmail.com>
2019-05-26 19:39:46 -0500

    agregando funcion de editar notas

--- src/controller/controller-firebase.js
+++ src/controller/controller-firebase.js
@@ -108,13 +108,25 @@ export const publish = (publishBy, publishText) => {
 //deleting notes
 export const delete_Notes = (id) =>{ 
   firebase.firestore().collection("notes").doc(id).delete()
-//   .then(function() {
-//   console.log("Document successfully deleted!");
-// })
-// .catch(function(error) {
-//   console.error("Error removing document: ", error);
-// });
 }
+export const edit_Note = (id) => {
+  document.querySelector(`#note-${id}`).readOnly = false;
+  document.querySelector(`#like-${id}`).style.display = 'none';
+  document.querySelector(`#edit-${id}`).style.display = 'none';
+  document.querySelector(`#save-${id}`).className += 'show-save';
+}
+
+export const save_Note = (id) => {
+  document.querySelector(`#note-${id}`).readOnly = true;
+  document.querySelector(`#like-${id}`).style.display = 'block';
+  document.querySelector(`#edit-${id}`).style.display = 'block';
+  document.querySelector(`#save-${id}`).className += 'hide-save';
+  const note_updated = document.querySelector(`#note-${id}`).value;
+  firebase.firestore().collection("notes").doc(id).update({
+    publishText: note_updated
+  });
+}
+
 //get username by its uid
 export const getName = (userId) => {
   return firebase.firestore().collection("users").doc(userId).get()
